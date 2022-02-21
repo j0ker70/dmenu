f8e4ba92cfb08949e9677c99cb5d6f2a128e6c97
K. M. Merajul Arefin <merajularefin@gmail.com>
2022-02-22 03:55:35 +0600

    change lineheight and font

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrains Mono Medium:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -38,7 +38,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 25;
+static unsigned int lineheight = 20;
 static unsigned int min_lineheight = 8;
 
 /*
